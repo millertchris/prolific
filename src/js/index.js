9bcac22ca9ed42b194bb8474a822e45ba8b48700
Chris Miller <miller.t.chris@gmail.com>
2021-02-06 21:02:26 -0500

    Adjusting top headline, block heights, and visually hiding checkbox for light switch

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import "../scss/main.scss";
 
 import animeBlocks from "./components/anime-blocks";
+import blockHeight from "./components/block-height";
 import colorSplash from "./components/color-splash";
 import darkMode from "./components/dark-mode";
 import formSubmit from "./components/form-submit";
@@ -9,21 +10,10 @@ import scrollTrigger from "./components/scroll-trigger";
 
 document.addEventListener("DOMContentLoaded", function () {
 	animeBlocks();
+	blockHeight();
 	colorSplash();
 	darkMode();
 	formSubmit();
 	particles();
 	scrollTrigger();
-
-	// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-	let vh = window.innerHeight * 0.01;
-	// Then we set the value in the --vh custom property to the root of the document
-	document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-	// We listen to the resize event
-	window.addEventListener("resize", () => {
-		// We execute the same script as before
-		let vh = window.innerHeight * 0.01;
-		document.documentElement.style.setProperty("--vh", `${vh}px`);
-	});
 });
